3d95dd04652d5c9f8935c9c08394d07ba20b7c0e
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-12-02 20:53:56 -0700

    added reusable states and game variables

---
+++ Assets/Scripts/GameVariables/GameVariables.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameVariables
+{
+    public static float MinVelocity = 0.09f;
+
+    public static float BirdColliderRadiusNormal = 0.54f;
+    public static float BirdColliderRadiusBig = 0.64f;
+}
