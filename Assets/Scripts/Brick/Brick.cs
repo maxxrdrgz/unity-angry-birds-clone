f31dd7bb89950c39753cb8240f31083a75faa671
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-12-02 20:46:46 -0700

    added the brick script and the scripts folder

---
+++ Assets/Scripts/Brick/Brick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public float health = 70f;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.GetComponent<Rigidbody2D>() == null)
+            return;
+
+        float damage = other.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10f;
+
+        if(damage > 10){
+            audioSource.Play();
+        }
+        health -= damage;
+
+        if(health <= 0)
+            Destroy(gameObject);
+    }
+}
