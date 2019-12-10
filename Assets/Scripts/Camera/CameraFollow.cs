ec59b37719515478cecf3f542b40af4f72ef3b95
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-12-09 20:26:15 -0700

    started working on camera follow script

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [HideInInspector]
+    public Vector3 startingPosition;
+
+    private float minCameraX = 0f, maxCameraX = 13f;
+    
+    [HideInInspector]
+    public bool isFollowing;
+
+    [HideInInspector]
+    public Transform birdToFollow;
+
+    void Awake()
+    {
+        startingPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if(isFollowing){
+            if(birdToFollow != null){
+                var birdPosition = birdToFollow.position;
+                float x = Mathf.Clamp(birdPosition.x, minCameraX, maxCameraX);
+                transform.position = new Vector3(x, startingPosition.y, startingPosition.z);
+
+            }else{
+                isFollowing = false;
+            }
+        }
+    }
+}
