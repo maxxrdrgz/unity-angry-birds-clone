2cf01d62595d56adeeee0ec5e50c8b3919b2c62c
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-12-10 20:56:17 -0700

    added parallax script to create parallax effect

---
+++ Assets/Scripts/Parallax/ParallaxScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScroll : MonoBehaviour
+{
+    public float parallaxFactor;
+    private Vector3 prevCameraPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevCameraPos = Camera.main.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 delta = Camera.main.transform.position - prevCameraPos;
+        delta.y = 0;
+        delta.z = 0;
+        transform.position += delta / parallaxFactor;
+
+        prevCameraPos = Camera.main.transform.position;
+    }
+}
