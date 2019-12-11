9e61f42bf883f6ff2d327a024fa2142fdc6a731c
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-12-10 20:56:02 -0700

    added script to destroy gameobjects when collided with

---
+++ Assets/Scripts/Destroyer/Destroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Bird" || other.tag == "Pig" || other.tag == "Brick"){
+            Destroy(other.gameObject);
+        }
+    }
+}
