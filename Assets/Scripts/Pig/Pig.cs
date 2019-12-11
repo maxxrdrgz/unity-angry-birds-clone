135f40a14ad2cebdb4949478d72fb1180695b706
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-12-10 21:51:49 -0700

    added pig script to control pig

---
+++ Assets/Scripts/Pig/Pig.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public float health = 150f;
+    public Sprite spriteShownWhenHurt;
+    private float changeSpriteHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        changeSpriteHealth = health - 30f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.GetComponent<Rigidbody2D>() == null){
+            return;
+        }
+
+        if(other.gameObject.tag == "Bird"){
+            audioSource.Play();
+            Destroy(gameObject);
+        }else{
+            float damage = other.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10f;
+
+            health -= damage;
+
+            if(damage >= 10){
+                audioSource.Play();
+            }
+
+            if(health < changeSpriteHealth){
+                gameObject.GetComponent<SpriteRenderer>().sprite = spriteShownWhenHurt;
+            }
+
+            if(health <= 0){
+                Destroy(gameObject);
+            }
+        }
+    }
+}
